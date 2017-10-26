5c0006556bd46818cd2ad06e02bbcd267f3a8010
Anders Fischer-Nielsen <andersfischern@me.com>
2017-10-26 21:00:53 +0200

    Added Google Maps package, custom map.

---
+++ src/components/MapView.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+const { withScriptjs, withGoogleMap, GoogleMap, Marker } = require("react-google-maps");
+
+export interface MapViewProperties { latitude: number, longitude: number }
+export class MapView extends React.Component<MapViewProperties, {}> {
+    render() {
+        let coordinates = { lat: this.props.latitude, lng: this.props.longitude };
+        let TrackMeMap = withScriptjs(withGoogleMap((props:any) => (
+            <GoogleMap defaultZoom={15} defaultCenter={coordinates}>
+                {props.isMarkerShown && <Marker position={coordinates} />}
+            </GoogleMap>
+        )));
+        return (
+            <TrackMeMap
+                isMarkerShown={true}
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
+        );
+    }
+}
