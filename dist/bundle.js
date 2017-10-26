fb0640ed5cbcf65b97f32a032a5edd3498ae6576
Anders Fischer-Nielsen <andersfischern@me.com>
2017-10-26 17:26:11 +0200

    Initial changes from initial scaffolding.

--- dist/bundle.js
+++ dist/bundle.js
@@ -79,7 +79,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(0);
 var ReactDOM = __webpack_require__(2);
 var Hello_1 = __webpack_require__(3);
-ReactDOM.render(React.createElement(Hello_1.Hello, { compiler: "TypeScript", framework: "React" }), document.getElementById("example"));
+ReactDOM.render(React.createElement(Hello_1.Hello, { compiler: "TypeScript", framework: "React" }), document.getElementById("container"));
 
 
 /***/ }),
