b10832e571965903ee647d4e73517b3874b601f7
Anders Fischer-Nielsen <andersfischern@me.com>
2017-11-06 20:27:04 +0100

    Initial implementation of  token based auth.

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { UsernameHeading } from "./Hello"
 import { MapView } from "./MapView"
 import { NearbySights } from "./NearbySights";
+import { Login } from "./Login";
 
 export interface Data {
     username: string;
@@ -13,9 +14,12 @@ export class Container extends React.Component<ContainerProperties, {}> {
     render() {
         return (
             <div>
-                <UsernameHeading username={this.props.data.username} />
-                <MapView latitude={this.props.data.latitude} longitude={this.props.data.longitude} />
-                <NearbySights latitude={this.props.data.latitude} longitude={this.props.data.longitude} />
+                <Login />
+                {
+                    /* <UsernameHeading username={this.props.data.username} />
+                    <MapView latitude={this.props.data.latitude} longitude={this.props.data.longitude} />
+                    <NearbySights latitude={this.props.data.latitude} longitude={this.props.data.longitude} /> */
+                }
             </div>
         );
     }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,26 +1,73 @@
 import * as React from "react";
 
-export class Login extends React.Component<{}, {}> {
+export interface ILoginState {
+    username: string,
+    password: string,
+    remember: boolean
+} 
+
+interface ITokens {
+    access_token: string,
+    refresh_token: string
+}
+
+export class Login extends React.Component<{}, ILoginState> {
     constructor(props:any) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);        
+        this.handleChange = this.handleChange.bind(this);     
     }
 
-    handleSubmit(event:any) {
+    async handleSubmit(event:any) {
         event.preventDefault();
+        let headers = new Headers({
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin": "no-cors"
+        });
+
+        let options = {
+            method: 'POST', 
+            body: JSON.stringify(this.state), 
+            headers: headers
+        };
+
+        try {
+            let response = await fetch('http://localhost:5000/auth', options);
+            if (response.status == 401) return;
+            let json = await response.json() as ITokens;
+            this.storeTokens(json);
+        } catch {
+            //TODO: Do things when error occures.
+        }
+    }
+
+    storeTokens(tokens:ITokens) {
+        if (tokens === undefined) return;
+        let storage = this.state.remember ? localStorage : sessionStorage;
+        let access_token_key = `TrackMeTraveling:${this.state.username}:access_token`;
+        let refresh_token_key = `TrackMeTraveling:${this.state.username}:refresh_token`;
+        storage.setItem(access_token_key, tokens.access_token);
+        storage.setItem(refresh_token_key, tokens.refresh_token);
+    }
+
+    handleChange(event:any) {
+        let target = event.target;
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        this.setState({[target.name]: value});
     }
     
     render() {
         return (
             <div>
                 <h1>Login</h1>
-                <form id="loginForm" onSubmit={this.handleSubmit}> 
-                <label>Username
-                    <input type="text"></input>
+                <form onSubmit={this.handleSubmit}> 
+                <input type="text" name="username" placeholder="Username" onChange={this.handleChange}></input>
+                <input name="password" placeholder="Password" type="password" onChange={this.handleChange}></input>
+                <label> Remember me
+                    <input name="remember" type="checkbox" onChange={this.handleChange}></input>
                 </label>
-                <label>Password
-                    <input type="password"></input>
-                </label>
+                <input type="submit" value="Submit"></input>
                 </form>
             </div>
         );
